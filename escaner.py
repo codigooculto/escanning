6920d90bb0465156f6f9ab6bfae2864a7d80c572
codigooculto <56204500+codigooculto@users.noreply.github.com>
2020-11-01 03:51:25 +0000

    Update escaner.py

--- escaner.py
+++ escaner.py
@@ -2,36 +2,43 @@ from colorama import *
 import eye
 import json
 import nmap3
+import datetime
 
-def escaneo():
-    global ip
-    nmap = nmap3.Nmap()
-    ip = str(input("ingrese ip o host a escanear >_ "))
-    print(Fore.CYAN + "ESCANEANDO..."+Fore.RESET+"")
-
-    resultados = nmap.scan_top_ports(ip)
-    #print(resultados)#muestra los resultados en formato json
-    #guardando los datos en un archivo json
-    cadena = json.dumps(resultados)
-    with open('escaneo.json','w') as f:
-        json.dump(resultados,f)
-
-def lectura():
-    #ordenando los datos json
-    with open('escaneo.json') as datos:
-        Datos = json.loads(datos.read())
-    i = 0
-
-    print(""" \n\n
+class Pymap:
+    def __init__(self):
+        global x #variable global
+        global ip #variable global
+        self.x = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        self.nmap = nmap3.Nmap()
+        self.ip = str(input("ingrese ip o host a escanear: "))
+        print(Fore.CYAN + "Escaneando..."+Fore.RESET+"")
+        self.resultados = self.nmap.scan_top_ports(self.ip)
+        #print(self.resultados)
+        
+        #guardando los datos en un archivo json
+        
+        self.cadena = json.dumps(self.resultados)
+        with open('json/escaneo1_{}.json'.format(self.x),'w') as self.f:
+            json.dump(self.resultados,self.f)
+            
+    def lectura(self):
+        #ordenando los datos en json
+        with open('json/escaneo1_{}.json'.format(self.x)) as self.datos:
+            self.Datos = json.loads(self.datos.read())
+        
+        self.i = 0
+        print(""" \n\n
 \t\t\tINICIO_________________________ \n\n
- """, Datos[ip][0])
-    try:
-        while i <=65535:
-            i += 1
-            print(Datos[ip][i],"\n")
-    except IndexError:
-        print(Fore.GREEN+"escaneo completo"+Fore.RESET+"")
+ """, self.Datos[self.ip][0])
+        try:
+            while self.i <=65535:
+                self.i += 1
+                self.imprime = print(self.Datos[self.ip][self.i],"\n")
 
-if __name__ == "__main__":
-    escaneo()
-    lectura()
+        except IndexError:
+            print(Fore.GREEN+"escaneo completo"+Fore.RESET+"")
+            
+            
+ejecucion = Pymap()#se ejecuta luego que se crea un objeto de la clase Pymap
+
+ejecucion.lectura()
